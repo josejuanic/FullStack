02cb8e86adb59ae3d35ff269cb2359b02d8e3787
IBANEZ <josejuan.ic.dim@gmail.com>
2026-06-11 12:11:05 +0200

    Commit #2: Ejercicio2

--- src/main/java/com/example/fullstack/controllers/TareaController.java
+++ src/main/java/com/example/fullstack/controllers/TareaController.java
@@ -1,6 +1,7 @@
 package com.example.fullstack.controllers;
 
 import com.example.fullstack.models.Tarea;
+import com.example.fullstack.services.TareaService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,17 @@ import java.util.List;
 
 @RestController
 public class TareaController {
-    @GetMapping("/tareas")
-    public List<Tarea> getTareas() {
-        return List.of(new Tarea("tarea1", 1, true
-        ), new Tarea("tarea2", 2, true), new Tarea("tarea3", 3, false));
+
+    private final TareaService tareaService;
+
+    public TareaController(TareaService tareaService) {
+        this.tareaService=tareaService;
     }
+
+    @GetMapping("/tareas")
+    public List<Tarea> getTareas(){
+        return tareaService.getTareas();
+    }
+
+
 }
