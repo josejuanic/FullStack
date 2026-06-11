5870bb72e7d1a629a8507723035f4b952e9a5a9c
IBANEZ <josejuan.ic.dim@gmail.com>
2026-06-11 13:32:22 +0200

    Commit #3: Ejercicio3

--- src/main/java/com/example/fullstack/services/TareaService.java
+++ src/main/java/com/example/fullstack/services/TareaService.java
@@ -4,16 +4,42 @@ import com.example.fullstack.models.Tarea;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TareaService {
     private List<Tarea> tareas;
+    private Long siguienteId = 3L;
+
     public TareaService(){
-        tareas = List.of(new Tarea("tarea1", 1, true
-        ), new Tarea("tarea2", 2, true), new Tarea("tarea3", 3, false));
+        tareas = List.of(
+                new Tarea("tarea1", 1, true),
+                new Tarea("tarea2", 2, true),
+                new Tarea("tarea3", 3, false)
+        );
     }
     public List<Tarea> getTareas() {
         return tareas;
     }
+    public Tarea crearTarea(Tarea tarea){
+        tarea.setId(siguienteId++);
+        tareas.add(tarea);
+        return tarea;
+    }
 
+    public Optional<Tarea> actualizarTarea(Long id, Tarea nuevaTarea){
+        return tareas.stream()
+                .filter(t -> t.getId().equals(id))
+                .findFirst()
+                .map(tarea -> {
+                    tarea.setTitulo(nuevaTarea.getTitulo());
+                    return tarea;
+                });
+    }
+
+    public boolean eliminarTarea(Long id){
+        return tareas.removeIf(
+                tarea -> tarea.getId().equals(id)
+        );
+    }
 }
